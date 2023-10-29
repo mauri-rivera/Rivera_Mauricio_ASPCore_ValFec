0b0d10534e1e7f6b13e81a11a6748b61b766598e
mauri-rivera <mauri.rivera@outlook.com>
2023-10-29 00:55:53 -0300

    update3

--- ValFec/Models/Fecha.cs
+++ ValFec/Models/Fecha.cs
@@ -6,8 +6,10 @@ namespace ValFec.Models;
 public class Fecha
 {
     [FutureDate]
+    [DataType(DataType.Date, ErrorMessage = "Proporcione una fecha")]
     public DateTime PruebaFecha { get; set; }
 
     [FutureDate]
+    [DataType(DataType.DateTime, ErrorMessage = "Proporcione una fecha con hora")]
     public DateTime PruebaFechaHora { get; set; }
 }
